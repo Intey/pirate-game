c883694f3eb15ec109c9d293a0b17c86fcf8af11
Intey <ziexe0@gmail.com>
2018-08-28 09:25:06 +0300

    [up] немного доков

---
+++ HOWTO_NEW_PROJECT.rst
@@ -0,0 +1,11 @@
+Как начать новый проект на cocos2d-x
+====================================
+
+.. code-block:: bash
+  git clone https://github.com/cocos2d/cocos2d-x.git
+  git checkout cocos-2d-x-3.17
+  git submodule update --init
+  python2 setup.py
+  # перезапустить шелл
+  cocos new
+
--- piratia/Classes/ForgeScene.cpp
+++ piratia/Classes/ForgeScene.cpp
@@ -1,13 +1,14 @@
 #include "ForgeScene.h"
 #include <cocos/2d/CCSprite.h>
 
-#include <cocos/ui/UIListView.h>
-#include <cocos/ui/UIVBox.h>
-#include <cocos/ui/UIHBox.h>
-#include <cocos/ui/UIText.h>
-#include <cocos/ui/UILayoutManager.h> // for Margin
 #include <cocos/2d/CCLayer.h>
 #include <cocos/ui/UIButton.h>
+#include <cocos/ui/UIListView.h>
+#include <cocos/ui/UILoadingBar.h> // используем как шкалу опыта
+#include <cocos/ui/UIHBox.h>
+#include <cocos/ui/UIText.h>
+#include <cocos/ui/UIVBox.h>
+
 #include "../Game/Recipe.h"
 #include "../Game/Player.h"
 
@@ -37,9 +38,6 @@ void ForgeScene::onEnter()
 bool ForgeScene::initAll()
 {
     // create gui
-    //
-    auto player = Game::Player::getInstance();
-
     auto rootLayout = ui::VBox::create();
     if (!rootLayout)
     {
@@ -49,7 +47,7 @@ bool ForgeScene::initAll()
     rootLayout->setAnchorPoint({0, 1});
     rootLayout->setPosition({0, 320});
 
-
+    auto player = Game::Player::getInstance();
     auto recipesView = renderRecipesList(player->getCraftAbility()->recipes());
     if (!recipesView) {
         CCLOGERROR("RENDER");
@@ -57,10 +55,10 @@ bool ForgeScene::initAll()
     }
 
     rootLayout->addChild(recipesView);
-    auto layer = LayerColor::create({100, 100, 255, 50});
+    auto layer = LayerColor::create({255, 220, 100, 50});
     layer->setAnchorPoint({0, 0});
+    layer->addChild(rootLayout);
 
-    this->addChild(rootLayout);
     this->addChild(layer);
     return true;
 }
@@ -71,6 +69,12 @@ ui::Widget* ForgeScene::renderRecipesList(std::vector<Game::Recipe> const& recip
     recipesView->setDirection(ui::ListView::Direction::VERTICAL);
     recipesView->setAnchorPoint({0, 0});
     recipesView->setPosition({100, 320});
+    recipesView->setPadding(5, 5, 0, 5);
+    // расстояние между элементами списка
+    recipesView->setItemsMargin(5);
+    // размер списка задается жестко, т.к. список может быть бесконечной длинны и
+    // ширины в худшем случае, поэтому его необходимо ограничить. А Layout не умеет
+    // как в Qt - автоматический размер, политики расширения и все такое
     recipesView->setContentSize({100, 300});
     if (!recipesView)
     {
@@ -100,7 +104,6 @@ ui::Widget* ForgeScene::renderRecipesList(std::vector<Game::Recipe> const& recip
 
 ui::Widget *ForgeScene::renderRecipeItem(const Game::Recipe &recipe) const
 {
-    int const MARGIN = 5;
     auto text1 = ui::Text::create(recipe.name(), "fonts/arial.ttf", 14);
     if (!text1)
     {
@@ -108,11 +111,6 @@ ui::Widget *ForgeScene::renderRecipeItem(const Game::Recipe &recipe) const
         return text1;
     }
     text1->setTextColor({255, 0, 255, 255});
-    auto margin1 = ui::Margin(MARGIN, 0, MARGIN, MARGIN);
-    auto lp = ui::LinearLayoutParameter::create();
-    if (!lp) CCLOGERROR("lp not");
-    lp->setMargin(margin1);
-    text1->setLayoutParameter(lp);
     return text1;
 }
 
